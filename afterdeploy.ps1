5780f664861886dbdf0e511201519471811467d8
vijai <vijai.djearam@gmail.com>
2021-06-10 17:33:38 +0200

    Update afterdeploy.ps1

--- afterdeploy.ps1
+++ afterdeploy.ps1
@@ -1,3 +1,6 @@
+$ipaddr = ((Test-NetConnection).SourceAddress).IPAddress
+$interfaceindex = (get-NetIPAddress -IPAddress $ipaddr).ifIndex
+Set-DnsClientServerAddress -InterfaceIndex $interfaceindex -ResetServerAddresses
 choco install microsoft-teams.install -y --params "'/AllUsers /NoAutoStart'"
 net localgroup "Utilisateurs du Bureau à distance" "985-Student" /ADD
 net localgroup "Utilisateurs du Bureau à distance" "985-Faculty" /ADD
