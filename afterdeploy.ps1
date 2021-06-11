64093f86ae52796509e7b14a683e1f8df047c280
vijai <vijai.djearam@gmail.com>
2021-06-11 15:09:26 +0200

    Update afterdeploy.ps1

--- afterdeploy.ps1
+++ afterdeploy.ps1
@@ -2,9 +2,11 @@ $ipaddr = ((Test-NetConnection).SourceAddress).IPAddress
 $interfaceindex = (get-NetIPAddress -IPAddress $ipaddr).ifIndex
 Set-DnsClientServerAddress -InterfaceIndex $interfaceindex -ResetServerAddresses
 choco install microsoft-teams.install -y --params "'/AllUsers /NoAutoStart'"
-net localgroup "Utilisateurs du Bureau à distance" "985-Student" /ADD
-net localgroup "Utilisateurs du Bureau à distance" "985-Faculty" /ADD
-net localgroup "Utilisateurs du Bureau à distance" "985-Employee" /ADD
+$Group = Get-WmiObject -Query "Select * From Win32_Group Where LocalAccount = TRUE And SID = 'S-1-5-32-555'"
+$GroupName = $Group.Name
+net localgroup $GroupName "985-Student" /ADD
+net localgroup $GroupName "985-Faculty" /ADD
+net localgroup $GroupName "985-Employee" /ADD
 net localgroup "Administrateurs" "985-Student" /ADD
 net localgroup "Administrateurs" "985-Faculty" /ADD
 net localgroup "Administrateurs" "985-Employee" /ADD
